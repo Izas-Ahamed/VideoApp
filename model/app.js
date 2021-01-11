3624db59dddf5efd303b366a39b88d89af694d59
Izas-Ahamed <60055462+Izas-Ahamed@users.noreply.github.com>
2021-01-11 17:03:29 +0530

    First Commit

---
+++ model/app.js
@@ -0,0 +1,230 @@
+var msgCtn = document.getElementById("message-container");
+var msg = document.getElementById("message");
+var videoCtn = document.getElementById("video-container");
+var terminatebtn1 = document.getElementById("terminatebtn1");
+var terminatebtn2 = document.getElementById("terminatebtn2");
+var fidCtn = document.getElementById("fid-container");
+var footer = document.getElementById("footer");
+var incalctn = document.getElementById("incoming-call-container");
+var sendmsg = document.getElementById("send-message");
+var yourMsg = document.getElementById("your-msg");
+var chatctn = document.getElementById("chat-container");
+var idCtn = document.getElementById("id-container");
+
+chatctn.style.display="none";
+incalctn.style.display="none";
+videoCtn.style.display = "none";
+terminatebtn1.style.display = "none";
+terminatebtn2.style.display = "none";
+var ourId;
+var getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
+  const peer = new Peer();
+  peerList = [];
+  
+  peer.on("open", (id) => {
+    document.getElementById("our-id").value = id;
+    ourId=id;
+
+  });
+
+
+  window.addEventListener("load", (event) => {
+  document
+    .getElementById("connect-video")
+    .addEventListener("click", (event) => {
+      let friendId = document.getElementById("friend-id").value;
+      if (friendId == "" || ourId == friendId) {
+        return;
+      }
+      document.getElementById("message").innerHTML = "Connecting : " + friendId;
+      connectPeerVideo(friendId);
+    });
+
+
+    document
+    .getElementById("connect-screen")
+    .addEventListener("click", (event) => {
+      let friendId = document.getElementById("friend-id").value;
+      if (friendId == "" || ourId == friendId) {
+        return;
+      }
+      document.getElementById("message").innerHTML = "Connecting : " + friendId;
+      connectPeerScreen(friendId);
+    });
+
+  peer.on("call", (call) => {
+  if(!peerList.includes(call.peer))
+  {
+    peerList.push(call.peer);
+
+  }
+  msg.style.display = "none";
+  incalctn.style.display="block";
+  terminatebtn1.style.display="block";
+  document.getElementById("accept-video").addEventListener('click', event =>
+  {
+    getUserMedia({ video: true, audio: true }, (stream) => {
+      call.answer(stream);
+      call.on("stream", (friendStream) => {
+          chatctn.style.display="block";
+          idCtn.style.display="none";
+          videoCtn.style.display = "block";
+          msgCtn.style.display = "none";
+          fidCtn.style.display = "none";
+          terminatebtn2.style.display = "block";
+          peerList.push(call.peer);
+          addPeerVideo(friendStream);
+      
+      });
+    });
+  });
+  
+  document.getElementById("accept-screen").addEventListener('click', event =>
+  {
+    navigator.mediaDevices.getDisplayMedia(
+      { video: {cursor: "always" },
+        audio: {echoCancellation: true,noiseSuppression: true}
+      }).then( stream =>
+        {
+        call.answer(stream);
+        call.on("stream", (friendStream) => {
+          videoCtn.style.display = "block";
+          chatctn.style.display="block";
+          idCtn.style.display="none";
+          msgCtn.style.display = "none";
+          fidCtn.style.display = "none";
+          terminatebtn2.style.display = "block";
+          peerList.push(call.peer);
+          addPeerVideo(friendStream);
+      
+      });
+        });
+  })
+
+  });
+
+  peer.on("connection", function (conn) {
+    conn.on("data", data => {
+      if(data=="#terminate-process")
+      {
+        setEndProperties();
+        document.getElementById("message").innerHTML = "User Disconnected !";
+        setTimeout(() => {
+          location.reload();
+        }, 3000);
+      }
+      else
+      {
+    document.getElementById("frnd-msg").textContent= data;
+      }
+    });
+  });
+
+  
+  function connectPeerVideo(id) {
+    getUserMedia({ video: true, audio: true }, (stream) => {
+      let call = peer.call(id, stream);
+      call.on("stream", (friendStream) => {
+        if (!peerList.includes(call.peer)) {
+          chatctn.style.display="block";
+          idCtn.style.display="none";
+          videoCtn.style.display = "block";
+          msgCtn.style.display = "none";
+          fidCtn.style.display = "none";
+          terminatebtn1.style.display = "none";
+          terminatebtn2.style.display = "block";
+          peerList.push(call.peer);
+          addPeerVideo(friendStream);
+        }
+      });
+    });
+  }
+
+
+function connectPeerScreen(id)
+  {
+
+    navigator.mediaDevices.getDisplayMedia(
+        { video: {cursor: "always" },
+          audio: {echoCancellation: true,noiseSuppression: true}
+        }).then(
+        stream => {
+            let call = peer.call(id, stream);
+            call.on("stream", (friendStream) => {
+              if (!peerList.includes(call.peer)) {
+                incalctn.style.display="none";
+                chatctn.style.display="block";
+                idCtn.style.display="none";
+                videoCtn.style.display = "block";
+                msgCtn.style.display = "none";
+                fidCtn.style.display = "none";
+                terminatebtn1.style.display = "none";
+                terminatebtn2.style.display = "block";
+                peerList.push(call.peer);
+                addPeerVideo(friendStream);
+              }
+            });
+
+        });
+    }
+
+  function addPeerVideo(stream) {
+    let video = document.createElement("video");
+    video.classList.add("video");
+    video.srcObject = stream;
+    video.play();
+    document.getElementById("video-container").append(video);
+  }
+
+  terminatebtn1.addEventListener("click", () => {
+    var conn = peer.connect(peerList[0]);
+    console.log("sdfdsf")
+    conn.on("open", () => {
+      conn.send("#terminate-process");
+      setEndProperties();
+      location.reload();
+    });
+  });
+
+  terminatebtn2.addEventListener("click", () => {
+    var conn = peer.connect(peerList[0]);
+    conn.on("open", () => {
+      conn.send("#terminate-process");
+      setEndProperties();
+      location.reload();
+    });
+  });
+document.getElementById("sendingmsg").addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    sendmsg.click();
+  }
+});
+  sendmsg.addEventListener("click", () => {
+    var conn = peer.connect(peerList[0]);
+    var msg = document.getElementById("sendingmsg")
+    conn.on("open", () => {
+      conn.send(msg.value);
+      yourMsg.textContent = msg.value;
+      document.getElementById("sendingmsg").value=""
+      });
+  });
+
+  function setEndProperties() {
+
+    videoCtn.style.display = "none";
+    incalctn.style.display="none";
+    terminatebtn1.style.display = "none";
+    terminatebtn2.style.display = "none";
+    msgCtn.style.display = "none";
+  }
+});
+
+function doCopy() {
+  var copyText = document.getElementById("our-id");
+  copyText.select();
+  document.execCommand("copy");
+}
